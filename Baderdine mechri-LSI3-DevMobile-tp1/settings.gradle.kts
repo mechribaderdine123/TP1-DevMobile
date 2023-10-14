590213dab36fbe96cd1c6e476e0eb95f6ba3ad97
bader mechri <badermechri02@gmail.com>
2023-10-14 06:09:35 -1200

    commit

---
+++ Baderdine mechri-LSI3-DevMobile-tp1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mechri-Baderdine-tp1"
+include(":app")
+ 
